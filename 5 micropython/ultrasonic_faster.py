3b8ea8a8aaa821816e172fc5e8bff595ca50712d
Alden Bradford <aldenbradford@gmail.com>
2022-02-06 22:50:49 -0500

    completed lab 5 draft

---
+++ 5 micropython/ultrasonic_faster.py
@@ -0,0 +1,32 @@
+import time
+from machine import Pin, time_pulse_us
+
+# these pins can be whatever -- just make sure
+# the wires correspond with the correct pins!
+trig = Pin(0, Pin.OUT)
+echo = Pin(1, Pin.IN)
+# this refers to the built in LED. Just to give some
+# visual indication of what is happening.
+led = Pin(25, Pin.OUT)
+
+trig.off()
+led.off()
+start = time.ticks_ms()
+
+while True:
+    # There has to be at least 60ms between measurements,
+    # for the sensor to reset itself.
+    time.sleep_ms(60)
+    led.on()
+    
+    # A 10 ms pulse tells the sensor to begin a measurement.
+    trig.on()
+    time.sleep_us(10)
+    trig.off()
+    
+    # This built in function measures how long a pulse is.
+    pulse = time_pulse_us(echo, 1, 3_000)
+    
+    now = time.ticks_ms() - start
+    print(now, pulse)
+    led.off()
