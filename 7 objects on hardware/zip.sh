7df1ee228a27b7fd79fb655fd9757cb46a02f516
Alden Bradford <aldenbradford@gmail.com>
2022-03-01 19:06:44 -0500

    accelerometer.py added to zip

--- 7 objects on hardware/zip.sh
+++ 7 objects on hardware/zip.sh
@@ -1,1 +1,1 @@
-zip lab7 lab7.ipynb button.py averager.py procedural_clock.py classy_clock.py grandfather_clock.py ssd1306.py lsm9ds0.py L7805CV.pdf TL1105.pdf
+zip lab7 lab7.ipynb button.py averager.py accelerometer.py procedural_clock.py classy_clock.py grandfather_clock.py ssd1306.py lsm9ds0.py L7805CV.pdf TL1105.pdf
