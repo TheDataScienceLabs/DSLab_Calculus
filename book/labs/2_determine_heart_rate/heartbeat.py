8fdf4b2ae23a1934fe974ff548a3b05041c8785e
Jaxson Pahukula <jaxpahu@gmail.com>
2023-01-24 14:43:26 -0500

    restore old heartbeat and main.py

--- book/labs/2_determine_heart_rate/heartbeat.py
+++ book/labs/2_determine_heart_rate/heartbeat.py
@@ -1,10 +1,10 @@
-"Heartbeat connection module"
-
 import serial
 from serial.tools import list_ports
+import numpy as np
 import time
 
-PICO_HWID = "2E8A:0005"  # Vendor Product ID for Raspberry Pi Pico
+PICO_HWID = "2E8A:0005"
+
 
 def get_pico_port():
     pico_ports = list(list_ports.grep(PICO_HWID))
@@ -12,26 +12,31 @@ def get_pico_port():
         raise Exception(
             "No Raspberry Pi Pico was detected. Check to make sure it is plugged in, and that no other programs are accessing it"
         )
-    return pico_ports[0].device # Full device name/path4
+    return pico_ports[0].device
 
 
-def serial_lines(port, num_samp, message, timeout):
+def serial_lines(port, timeout, message, ending):
     """
-    Listen to the port of the connected device and yield lines. If the serial buffer is empty, yield an empty string.
+    Send the given message on the port specified, and yield lines until
+    one of them matches ending. If the serial buffer is empty, yield an empty string.
     """
     with serial.Serial(port, timeout=timeout) as s:
-        # Add EOL control character (\n (LF), \r (CR), \r\n (CRLF)) to indicate that what is before will be sent.
-        # Then make an utf-8 encoded version of the string
         call = str.encode(message + "\r")
         s.write(call)
-        #response = s.readline()
+        response = s.readline()  # first line is always our message
+        if response != call + b"\n":
+            raise Exception(
+                f"""The microcontroller did not receive the command correctly.
+                We sent the message {call}, but we received the response {response}.
+                Is there something else reading from this serial port?"""
+            )
         line = s.readline().decode("UTF8").strip()
-        for k in range(num_samp):
+        while line != ending:
             yield line
             line = s.readline().decode("UTF8").strip()
-            
-            
-def acquire(rate, y, line, fig, num_samp, min_frame_time=0.1):
+
+
+def acquire(rate, y, line, fig, min_frame_time=0.1):
     """
     Take in heartbeat data, meanwhile updating the line and figure
     so that you can see the data as it arrives in real time.
@@ -40,16 +45,35 @@ def acquire(rate, y, line, fig, num_samp, min_frame_time=0.1):
     most_recent_draw = time.monotonic()
     for entry in serial_lines(
         get_pico_port(),
-        num_samp,
-        message = str(rate),
-        timeout = 0.5
+        timeout=0.005,
+        message=f"{rate},{len(y)}",
+        ending="done",
     ):
         if entry:
-            #y[i] = int(entry)
-            y[i] = int(''.join(c for c in entry if (c in "0123456789"))
+            y[i] = int(entry)
             i += 1
             line.set_ydata(y)
         if time.monotonic() - most_recent_draw > min_frame_time or not entry:
             # the buffer is empty, so we have time to draw.
             fig.canvas.draw()
             most_recent_draw = time.monotonic()
+
+
+def rolling_centered_average(x, n):
+    """
+    Compute a rolling average of n samples, centered. This fills the outside terms
+    where there are not enough neighbors with NaN, to preserve the shape.
+
+    arguments
+    ---------
+    x: array - the points to be averaged.
+    n: int - the number to include in each average.
+
+    returns
+    -------
+    out: array - the centered rolling averages, with NaN at either end filling our to the
+    same shape as x.
+    """
+    out = np.full_like(x, np.nan)
+    out[(n - 1) // 2 : -(n // 2)] = np.convolve(x, np.ones(n), mode="valid") / n
+    return out
---
+++ book/labs/2_determine_heart_rate/heartbeat_new.py
@@ -0,0 +1,55 @@
+"Heartbeat connection module"
+
+import serial
+from serial.tools import list_ports
+import time
+
+PICO_HWID = "2E8A:0005"  # Vendor Product ID for Raspberry Pi Pico
+
+def get_pico_port():
+    pico_ports = list(list_ports.grep(PICO_HWID))
+    if len(pico_ports) == 0:
+        raise Exception(
+            "No Raspberry Pi Pico was detected. Check to make sure it is plugged in, and that no other programs are accessing it"
+        )
+    return pico_ports[0].device # Full device name/path4
+
+
+def serial_lines(port, num_samp, message, timeout):
+    """
+    Listen to the port of the connected device and yield lines. If the serial buffer is empty, yield an empty string.
+    """
+    with serial.Serial(port, timeout=timeout) as s:
+        # Add EOL control character (\n (LF), \r (CR), \r\n (CRLF)) to indicate that what is before will be sent.
+        # Then make an utf-8 encoded version of the string
+        call = str.encode(message + "\r")
+        s.write(call)
+        #response = s.readline()
+        line = s.readline().decode("UTF8").strip()
+        for k in range(num_samp):
+            yield line
+            line = s.readline().decode("UTF8").strip()
+            
+            
+def acquire(rate, y, line, fig, num_samp, min_frame_time=0.1):
+    """
+    Take in heartbeat data, meanwhile updating the line and figure
+    so that you can see the data as it arrives in real time.
+    """
+    i = 0
+    most_recent_draw = time.monotonic()
+    for entry in serial_lines(
+        get_pico_port(),
+        num_samp,
+        message = str(rate),
+        timeout = 0.5
+    ):
+        if entry:
+            #y[i] = int(entry)
+            y[i] = int(''.join(c for c in entry if (c in "0123456789"))
+            i += 1
+            line.set_ydata(y)
+        if time.monotonic() - most_recent_draw > min_frame_time or not entry:
+            # the buffer is empty, so we have time to draw.
+            fig.canvas.draw()
+            most_recent_draw = time.monotonic()
